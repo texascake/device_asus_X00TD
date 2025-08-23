4e008c0683fbdc094975994480dcaaa59d1e8dee
Tiktodz <dotkit@electrowizard.me>
2025-08-23 17:05:03 +0000

    Reapply "sdm660-common: Change gesture and dt2w node to touchpanel"
    This reverts commit 6fba7cc7eac5e54dbc1bc459e83ca9615a5d013f.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -214,7 +214,7 @@ TARGET_BOARD_PLATFORM := sdm660
 
 # Power
 TARGET_USES_INTERACTION_BOOST := true
-TARGET_TAP_TO_WAKE_NODE := "/proc/tpd_gesture"
+TARGET_TAP_TO_WAKE_NODE := "/sys/kernel/touchpanel/dclicknode"
 
 # Properties
 TARGET_ODM_PROP += $(DEVICE_PATH)/properties/odm.prop
