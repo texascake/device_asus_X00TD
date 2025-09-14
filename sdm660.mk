336bfc7f473e547e5edd91d671869dd79e442f74
Tim Zimmermann <tim@linux4.de>
2025-09-14 20:43:48 +0700

    sdm660-common: Switch to AIDL camera HAL
    Change-Id: Idc9ccc33c4ba9c2c6668d145405da623cf4dfc49
    Signed-off-by: rsuntk <rsuntk@yukiprjkt.my.id>

--- sdm660.mk
+++ sdm660.mk
@@ -109,9 +109,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.camera.common@1.0 \
     android.hardware.camera.device@3.5 \
-    android.hardware.camera.provider@2.4-impl \
-    android.hardware.camera.provider@2.4-service \
-    android.hardware.camera.provider@2.6 \
+    android.hardware.camera.provider-service_32.lineage \
     vendor.qti.hardware.camera.device@1.0 \
     vendor.qti.hardware.camera.device@1.0.vendor \
     liblz4.vendor \
