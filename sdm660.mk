fed72728bc58be802e0434e9c25a774021b12de6
rsuntk <rsuntk@yukiprjkt.my.id>
2025-09-19 18:33:16 +0700

    sdm660-common: qcom-caf: Drop unused and etc
    FAILED:
    build/make/core/main.mk:1096: warning:  device/asus/X01BD/lineage_X01BD.mk includes non-existent modules in PRODUCT_PACKAGES
    Offending entries:
    libOmxAacEnc
    libOmxAmrEnc
    libOmxEvrcEnc
    libOmxG711Enc
    libOmxQcelp13Enc
    build/make/core/main.mk:1096: error: Build failed.
    09:54:45 ckati failed with: exit status 1
    
    Signed-off-by: rsuntk <rsuntk@yukiprjkt.my.id>

--- sdm660.mk
+++ sdm660.mk
@@ -325,12 +325,7 @@ BOARD_SHIPPING_API_LEVEL := 30
 PRODUCT_PACKAGES += \
     libc2dcolorconvert \
     libmm-omxcore \
-    libOmxAacEnc \
-    libOmxAmrEnc \
     libOmxCore \
-    libOmxEvrcEnc \
-    libOmxG711Enc \
-    libOmxQcelp13Enc \
     libOmxVdec \
     libOmxVenc \
     libstagefright_amrnb_common.vendor \
