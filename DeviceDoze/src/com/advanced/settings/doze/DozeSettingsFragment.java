5aaf91ea12baa2be083c5fcf36b551629865ef33
AnierinB <anierin@evolution-x.org>
2026-01-09 09:09:52 +0800

    X01BD: DeviceDoze: Move to PreferenceFragmentCompat
    PreferenceFragment was deprecated in API level 28.
    
    https://developer.android.com/jetpack/androidx/releases/preference#1.1.0
    
    Change-Id: Ie8a4b04bd588afbbbc5882349ddbc05f988f80bf

--- DeviceDoze/src/com/advanced/settings/doze/DozeSettingsFragment.java
+++ DeviceDoze/src/com/advanced/settings/doze/DozeSettingsFragment.java
@@ -23,7 +23,7 @@ import android.app.Dialog;
 import android.content.DialogInterface;
 import android.content.SharedPreferences;
 import android.os.Bundle;
-import androidx.preference.PreferenceFragment;
+import androidx.preference.PreferenceFragmentCompat;
 import androidx.preference.SwitchPreference;
 import androidx.appcompat.app.AlertDialog;
 import androidx.fragment.app.DialogFragment;
@@ -34,7 +34,7 @@ import android.widget.CompoundButton;
 
 import com.android.settingslib.widget.MainSwitchPreference;
 
-public class DozeSettingsFragment extends PreferenceFragment implements OnPreferenceChangeListener,
+public class DozeSettingsFragment extends PreferenceFragmentCompat implements OnPreferenceChangeListener,
         CompoundButton.OnCheckedChangeListener {
 
     private MainSwitchPreference mSwitchBar;
@@ -46,7 +46,7 @@ public class DozeSettingsFragment extends PreferenceFragment implements OnPrefer
 
     @Override
     public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
-        addPreferencesFromResource(R.xml.doze_settings);
+        setPreferencesFromResource(R.xml.doze_settings, rootKey);
 
         SharedPreferences prefs = getActivity().getSharedPreferences("doze_settings",
                 Activity.MODE_PRIVATE);
