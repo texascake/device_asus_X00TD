5f0278aee7d047428a9451ab241552ac37427c3a
rsuntk <rissu.ntk@gmail.com>
2026-02-09 20:34:33 +0700

    Revert "sdm660-common: Add fusefs cmdline"
    * Our kernel source is defaulted to FUSE instead of sdcardfs, thus, remove this obsolete cmdline workaround
    https://github.com/rsuntk/android_kernel_asus_sdm660-4.19/tree/lineage-23.2
    
    This reverts commit 47ed2f84bb1a3c28a0a6f23c59f2770c65c8c6bb.
    
    Signed-off-by: rsuntk <rissu.ntk@gmail.com>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -141,7 +141,7 @@ BOARD_HAVE_QCOM_FM := true
 
 # Kernel
 BOARD_KERNEL_BASE := 0x80000000
-BOARD_KERNEL_CMDLINE := androidboot.hardware=qcom user_debug=31 msm_rtb.filter=0x37 ehci-hcd.park=3 lpm_levels.sleep_disabled=1 sched_enable_hmp=1 sched_enable_power_aware=1 androidboot.boot_devices=soc/c0c4000.sdhci service_locator.enable=1 loop.max_part=7 use_fusefs=1
+BOARD_KERNEL_CMDLINE := androidboot.hardware=qcom user_debug=31 msm_rtb.filter=0x37 ehci-hcd.park=3 lpm_levels.sleep_disabled=1 sched_enable_hmp=1 sched_enable_power_aware=1 androidboot.boot_devices=soc/c0c4000.sdhci service_locator.enable=1 loop.max_part=7
 BOARD_KERNEL_CMDLINE += androidboot.init_fatal_reboot_target=recovery printk.devkmsg=on
 BOARD_KERNEL_CMDLINE += console=ttyMSM0,115200,n8 androidboot.console=ttyMSM0
 #BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
