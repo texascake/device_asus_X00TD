028cd37ff71d1af05ac34f87e598dff7958f9266
rsuntk <rissu.ntk@gmail.com>
2026-02-11 08:33:56 +0700

    sdm660-common: build libui-v34
    Signed-off-by: rsuntk <rissu.ntk@gmail.com>

--- sdm660.mk
+++ sdm660.mk
@@ -555,6 +555,10 @@ PRODUCT_PACKAGES += \
     libprotobuf-cpp-full-21.7 \
     libprotobuf-cpp-lite-21.7
 
+# libui compat
+PRODUCT_PACKAGES += \
+    libui-v34
+
 # VNDK
 PRODUCT_PACKAGES += \
     libstdc++_vendor
