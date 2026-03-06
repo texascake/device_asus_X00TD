1c073ad06216e2120106afc6d603f1bdf0de9c79
texascake <tiktodz@soulvibe-team.cloud>
2026-03-06 19:36:28 +0000

    Revert "X01BD: Bringup to @ProjectInfinity-X"
    This reverts commit 48cf893b8d91a4909b08616200c93250a3def179.

--- lineage_X01BD.mk
+++ lineage_X01BD.mk
@@ -9,8 +9,8 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o_mr1.mk)
 
-# Inherit some common Infinity-X stuff
-$(call inherit-product, vendor/infinity/config/common_full_phone.mk)
+# Inherit some common LineageOS stuff
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 # Inherit from X01BD device
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
@@ -23,13 +23,8 @@ TARGET_FACE_UNLOCK_SUPPORTED := true
 TARGET_EXCLUDES_AUDIOFX := true
 TARGET_SUPPORTS_QUICK_TAP := false
 
-# Inherit common Infinity-X props
-INFINITY_BUILD_TYPE := UNOFFICIAL
-INFINITY_MAINTAINER := "rsuntk"
-WITH_GAPPS := false
-
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := infinity_X01BD
+PRODUCT_NAME := lineage_X01BD
 PRODUCT_DEVICE := X01BD
 PRODUCT_BRAND := asus
 PRODUCT_MODEL := Zenfone Max Pro M2
